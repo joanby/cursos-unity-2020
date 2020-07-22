7583e6c4940f995a4e53786a8d2f47e1ed7aecd4
Juan Gabriel <juangabriel@frogames.es>
2020-07-22 12:58:32 +0200

    Game Manager completado

---
+++ 05_QuickClick/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public List<GameObject> targetPrefabs;
+
+    public float spawnRate = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+            Instantiate(targetPrefabs[index]);
+        }
+    }
+    
+}
