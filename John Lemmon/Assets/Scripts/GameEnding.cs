4fe045ade5ba0ff2ed5b528fa7de50ea3595c99d
Juan Gabriel <juangabriel@frogames.es>
2020-07-29 08:51:27 +0200

    Actualizar John Lemon

---
+++ John Lemmon/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+
+    public float displayImageDuration = 1f;
+    
+    private bool isPlayerAtExit; 
+    
+    public GameObject player;
+
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    
+    private float timer;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            isPlayerAtExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerAtExit)
+        {
+            timer += Time.deltaTime;
+            exitBackgroundImageCanvasGroup.alpha = Mathf.Clamp(timer / fadeDuration,0,1);
+
+            if (timer > fadeDuration + displayImageDuration)
+            {
+                EndLevel();
+            }
+        }
+    }
+
+    
+    void EndLevel()
+    {
+        Application.Quit();
+    }
+}
---
+++ John Lemmon/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 movement;
+
+    private Animator _animator;
+
+    private Rigidbody _rigidbody;
+    
+    [SerializeField]
+    private float turnSpeed;
+
+    private Quaternion rotation = Quaternion.identity;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        movement.Set(horizontal, 0, vertical);
+        movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        _animator.SetBool("IsWalking", isWalking);
+
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward,
+            movement, turnSpeed * Time.fixedDeltaTime, 0f);
+        
+        rotation = Quaternion.LookRotation(desiredForward);
+
+    }
+
+    private void OnAnimatorMove()
+    {
+        //S = S0 + delta S
+        _rigidbody.MovePosition(_rigidbody.position + movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
+    }
+}
